7ab3480936edd6996d820c079cc2b6f5e8c42f7e
Joe Tortorelli <jtortorelli@ups.com>
2018-06-07 12:19:33 -0400

    hm

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var Twit = require('twit')
-let express = require('express');
-let T = new Twit({
-    consumer_key:         'zKp2Zpr1V5AJomHLqazcIJP16',
-    consumer_secret:      'DhvLm0StrU348LcFIxH1PuC6VZY56eoSI6HbvUPVeSlus8fZVt',
-    access_token:         '469590520-Lz6vO06uVgfN1tPkm7fE2ZE6nfy26WgSChCyZYQQ',
-    access_token_secret:  'pls5Hx99FBLJFvBWI7sYvSTDEfndOFlyzteAQLAaK91q5',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 let app = express();
-
+let twit = require('twit');
+let T = new Twit({
+  consumer_key:         'zKp2Zpr1V5AJomHLqazcIJP16',
+  consumer_secret:      'DhvLm0StrU348LcFIxH1PuC6VZY56eoSI6HbvUPVeSlus8fZVt',
+  access_token:         '469590520-Lz6vO06uVgfN1tPkm7fE2ZE6nfy26WgSChCyZYQQ',
+  access_token_secret:  'pls5Hx99FBLJFvBWI7sYvSTDEfndOFlyzteAQLAaK91q5',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
 app.get('/', (req, res) => { 
-    T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-        console.log(data)
-      })
-})
 
-app.listen(3000, () => { 
-    console.log('listening on port 3000');
 })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
