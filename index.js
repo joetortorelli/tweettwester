8da75e0e9341e1108f21b8a8ad2162bd252bcb52
Joe Tortorelli <jtortorelli@ups.com>
2018-06-07 12:25:39 -0400

    got it

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-let app = express();
-let twit = require('twit');
 let T = new Twit({
   consumer_key:         'zKp2Zpr1V5AJomHLqazcIJP16',
   consumer_secret:      'DhvLm0StrU348LcFIxH1PuC6VZY56eoSI6HbvUPVeSlus8fZVt',
@@ -10,14 +8,17 @@ let T = new Twit({
   access_token_secret:  'pls5Hx99FBLJFvBWI7sYvSTDEfndOFlyzteAQLAaK91q5',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
-app.get('/', (req, res) => { 
-  var stream = T.stream('statuses/filter', { track: 'mango' })
-  T.get('search/tweets', { q: 'banana since:2011-07-11', count: 10 }, function(err, data, response) {
-    console.log(data)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => { 
+    var stream = T.stream('statuses/filter', { track: 'mango' })
+    T.get('search/tweets', { q: 'banana since:2011-07-11', count: 10 }, function(err, data, response) {
+     res.send(data);
+    })
+    stream.on('tweet', function (tweet) {
+      console.log(tweet)
+    })
   })
-  stream.on('tweet', function (tweet) {
-    console.log(tweet)
-  })
-  
-})
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
