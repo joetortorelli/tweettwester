ccf6545490bb756f85deb414c6b2581c40d0b314
Admin <admin@Admins-Mac-mini.home>
2018-07-21 14:45:01 -0400

    hmmm

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ express().get('/' + process.env.hidden, (req, res) => {
     //     // do something with apps
     //     console.log('is this gud: ' + JSON.stringify(apps));
     //   })
-    heroku.post('https://api.heroku.com/apps/tweettwester/config-vars', {body: {"handlesToCheck": "@UPS","AnotherTest": "qux"}}).then(app => {
+    heroku.post('/apps/tweettwester/config-vars', {body: {"handlesToCheck": "@UPS","AnotherTest": "qux"}}).then(app => {
         console.log('we gud?');
-      res.send('we gud?');
     })
+    res.send('we gud?');
 })
 .listen(PORT, () => { console.log(`Listening on ${ PORT }`) });
